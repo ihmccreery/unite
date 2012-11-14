456c0f0faef259576a6c99783817b0e22a0e0a3b
Isaac Hollander McCreery <ihm@cs.oberlin.edu>
2012-11-14 00:41:25 -0500

    initial Rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Constellate::Application.config.secret_token = 'abecb347963f57b9b9be98a8bedf357b80b713e74ddb4ffabec0dab8dfdb67dfab39d84012a6aa29d37ce2b733008d55559374dd15999d8f96143d1779d0c3e4'
