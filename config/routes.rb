bf845b10f0f2fe0c848c766378494ffd91dda0cc
Isaac Hollander McCreery <ihm@cs.oberlin.edu>
2012-11-17 18:17:54 -0500

    made organizations the root resource and removed default index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Constellate::Application.routes.draw do
-  resources :organizations
 
+  resources :organizations, path: ''
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
