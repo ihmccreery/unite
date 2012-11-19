a20570e53736e65957b5d9b1024f42a22d49fd92
Isaac Hollander McCreery <ihm@cs.oberlin.edu>
2012-11-19 11:16:18 -0500

    made slug keyword errors on slug field, and fixed tests appropriately

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,6 @@
 class Organization < ActiveRecord::Base
 
+  # friendly_id uses slug
   include FriendlyId
   friendly_id :slug
 
@@ -7,5 +8,13 @@ class Organization < ActiveRecord::Base
 
   validates :title, :description, :slug, presence: true
   validates :slug, format: { with: /^[0-9a-zA-Z_\-]+$/ }, uniqueness: true
+
+  # move friendly_id errors to slug
+  after_validation :move_friendly_id_error_to_slug
+
+  # move friendly_id error to slug
+  def move_friendly_id_error_to_slug
+    errors.add :slug, *errors.delete(:friendly_id) if errors[:friendly_id].present?
+  end
 
 end
