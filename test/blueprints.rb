5fb5f0b0c5de3caef76cb576f24ddfe8b9cbf144
Isaac Hollander McCreery <ihm@cs.oberlin.edu>
2012-11-28 11:33:13 -0500

    first pass at user tests, working

--- test/blueprints.rb
+++ test/blueprints.rb
@@ -7,3 +7,10 @@ require 'machinist/active_record'
 #     title { "Post #{sn}" }
 #     body  { "Lorem ipsum..." }
 #   end
+
+User.blueprint do |user|
+  username { "user#{sn}" }
+  password  { "password#{sn}" }
+  email  { "user#{sn}@example.com" }
+  confirmed_at { Time.now }
+end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,4 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  context "a factory user" do
+    setup do
+      @u = User.make!
+    end
+    should "be valid" do
+      assert @u.valid?
+    end
+  end
+
 end
