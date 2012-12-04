2cb08e60845fe97780ee4489182bc8dc042082c2
Isaac Hollander McCreery <ihm@cs.oberlin.edu>
2012-12-04 10:25:35 -0500

    added Grant's set_current_user to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # for Grant
+  before_filter :set_current_user
+
+  # for Grant
+  private
+  def set_current_user
+    Grant::User.current_user = @current_user
+  end
+
 end
