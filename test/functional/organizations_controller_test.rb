03c058c422752e45594d6454fb6dc3ddafcef1d9
Isaac Hollander McCreery <ihm@cs.oberlin.edu>
2012-12-04 12:02:12 -0500

    changed functional tests to be Shoulda-based

--- test/functional/organizations_controller_test.rb
+++ test/functional/organizations_controller_test.rb
@@ -2,105 +2,109 @@ require 'test_helper'
 
 class OrganizationsControllerTest < ActionController::TestCase
 
-  setup do
-    @o = Organization.make!
-    @attributes = {title: "Some Organization",
-                   slug: "so",
-                   description: "We do stuff."}
-    sign_in @u = User.make!
-  end
+  context "a signed-in factory user with a factory organization" do
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:organizations)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create organization" do
-    assert_difference('Organization.count') do
-      post :create, organization: @attributes
+    setup do
+      @o = Organization.make!
+      @attributes = {title: "Some Organization",
+                     slug: "so",
+                     description: "We do stuff."}
+      sign_in @u = User.make!
     end
 
-    assert_redirected_to organization_path(assigns(:organization))
-  end
-
-  test "should show organization" do
-    get :show, id: @o
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @o
-    assert_response :success
-  end
-
-  test "should update organization" do
-    put :update, id: @o, organization: @attributes
-    assert_redirected_to organization_path(assigns(:organization))
-  end
-
-  test "should destroy organization" do
-    assert_difference('Organization.count', -1) do
-      delete :destroy, id: @o
+    should "get index" do
+      get :index
+      assert_response :success
+      assert_not_nil assigns(:organizations)
     end
 
-    assert_redirected_to organizations_path
-  end
-
-  test "should join organization" do
-    assert_difference('Membership.count') do
-      post :join, id: @o
+    should "get new" do
+      get :new
+      assert_response :success
     end
 
-    assert_redirected_to organization_path(assigns(:organization))
-  end
+    should "create organization" do
+      assert_difference('Organization.count') do
+        post :create, organization: @attributes
+      end
 
-  test "should leave organization" do
-    @u.join(@o)
-    assert_difference('Membership.count', -1) do
-      delete :leave, id: @o
+      assert_redirected_to organization_path(assigns(:organization))
     end
 
-    assert_redirected_to organization_path(assigns(:organization))
-  end
-
-  test "should watch organization" do
-    assert_difference('Watch.count') do
-      post :watch, id: @o
+    should "show organization" do
+      get :show, id: @o
+      assert_response :success
     end
 
-    assert_redirected_to organization_path(assigns(:organization))
-  end
-
-  test "should unwatch organization" do
-    @u.watch(@o)
-    assert_difference('Watch.count', -1) do
-      delete :unwatch, id: @o
+    should "get edit" do
+      get :edit, id: @o
+      assert_response :success
     end
 
-    assert_redirected_to organization_path(assigns(:organization))
-  end
-
-  test "should star organization" do
-    assert_difference('Star.count') do
-      post :star, id: @o
+    should "update organization" do
+      put :update, id: @o, organization: @attributes
+      assert_redirected_to organization_path(assigns(:organization))
     end
 
-    assert_redirected_to organization_path(assigns(:organization))
-  end
+    should "destroy organization" do
+      assert_difference('Organization.count', -1) do
+        delete :destroy, id: @o
+      end
 
-  test "should unstar organization" do
-    @u.star(@o)
-    assert_difference('Star.count', -1) do
-      delete :unstar, id: @o
+      assert_redirected_to organizations_path
     end
 
-    assert_redirected_to organization_path(assigns(:organization))
+    should "join organization" do
+      assert_difference('Membership.count') do
+        post :join, id: @o
+      end
+
+      assert_redirected_to organization_path(assigns(:organization))
+    end
+
+    should "leave organization" do
+      @u.join(@o)
+      assert_difference('Membership.count', -1) do
+        delete :leave, id: @o
+      end
+
+      assert_redirected_to organization_path(assigns(:organization))
+    end
+
+    should "watch organization" do
+      assert_difference('Watch.count') do
+        post :watch, id: @o
+      end
+
+      assert_redirected_to organization_path(assigns(:organization))
+    end
+
+    should "unwatch organization" do
+      @u.watch(@o)
+      assert_difference('Watch.count', -1) do
+        delete :unwatch, id: @o
+      end
+
+      assert_redirected_to organization_path(assigns(:organization))
+    end
+
+    should "star organization" do
+      assert_difference('Star.count') do
+        post :star, id: @o
+      end
+
+      assert_redirected_to organization_path(assigns(:organization))
+    end
+
+    should "unstar organization" do
+      @u.star(@o)
+      assert_difference('Star.count', -1) do
+        delete :unstar, id: @o
+      end
+
+      assert_redirected_to organization_path(assigns(:organization))
+    end
+
   end
 
 end
