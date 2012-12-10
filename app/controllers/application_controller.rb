fd0947a432aa6a88a2aec38e7fc9628f8915f57f
Isaac Hollander McCreery <ihm@cs.oberlin.edu>
2012-12-10 11:25:04 -0500

    fixed bug that caused redirect to /users/sign_up after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,15 +15,16 @@ class ApplicationController < ActionController::Base
 
   # for Devise redirect back to page
   def store_location
-    session[:previous_urls] ||= []
-    # store unique urls only
-    session[:previous_urls].prepend request.fullpath if session[:previous_urls].first != request.fullpath
-    session[:previous_urls].pop if session[:previous_urls].count > 2
+    # store last url as long as it isn't a /users path
+    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
   end
 
-  # for Devise redirect back to page
   def after_sign_in_path_for(resource)
-    session[:previous_urls].last || root_path
+    session[:previous_url] || root_path
+  end
+
+  def after_update_path_for(resource)
+    session[:previous_url] || root_path
   end
 
 end
