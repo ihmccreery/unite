700d3b03b61083ada328689ca332dc855f6908c6
Isaac Hollander McCreery <ihm@cs.oberlin.edu>
2012-12-18 01:32:49 -0500

    changed security so that an organization's member can destroy stars, watches, and memberships for that organization

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,7 +2,7 @@ class Membership < ActiveRecord::Base
 
   grant(:find) { true }
   grant(:create) { |user, membership| user && membership.organization.has_member?(user) }
-  grant(:destroy) { |user, membership| user && (membership.user == user) }
+  grant(:destroy) { |user, membership| user && ((membership.user == user) || (membership.organization.has_member?(user))) }
 
   belongs_to :user
   belongs_to :organization
