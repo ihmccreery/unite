e7f92fa6cde42f1ad35f5b3475b57bff2a9e639b
Isaac Hollander McCreery <ihm@cs.oberlin.edu>
2012-12-17 22:20:30 -0500

    added user integration tests

---
+++ test/integration/user_flows_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+class UserFlowsTest < ActionDispatch::IntegrationTest
+
+  context "the Anonymous user" do
+
+    should "get the homepage" do
+      get "/"
+      assert_response :success
+    end
+
+    should "get the sign in page" do
+      get "/users/sign_in"
+      assert_response :success
+    end
+
+  end
+
+  context "a user" do
+
+    setup do
+      without_grant do
+        @u = User.make!
+      end
+    end
+
+    should "not sign in with the incorrect password" do
+      post_via_redirect "/users/sign_in", user: { username: @u.username, password: 'bad password' }
+      assert_response :success
+      assert_equal I18n.t(:invalid, scope: [:devise, :failure]), flash[:alert]
+    end
+
+    should "sign in with the correct password" do
+      post_via_redirect "/users/sign_in", user: { username: @u.username, password: @u.password }
+      assert_response :success
+      assert_equal I18n.t(:signed_in, scope: [:devise, :sessions]), flash[:notice]
+      assert_equal @u, Grant::User.current_user
+    end
+
+    should "sign in and then sign out" do
+      post_via_redirect "/users/sign_in", user: { username: @u.username, password: @u.password }
+      delete_via_redirect "/users/sign_out"
+      assert_response :success
+      assert_equal I18n.t(:signed_out, scope: [:devise, :sessions]), flash[:notice]
+      assert_equal nil, Grant::User.current_user
+    end
+
+  end
+
+end
